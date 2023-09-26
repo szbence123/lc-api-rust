20ba02ab6dfe43332d04ae01a6c62103324e7909
Bence Szalai <szbence123@gmail.com>
2023-09-26 03:00:27 +0200

    formattings

--- src/main.rs
+++ src/main.rs
@@ -23,26 +23,27 @@ async fn main() -> std::io::Result<()> {
 
     HttpServer::new(move || {
         App::new()
+            /* PERSON COLLECTION */
             .app_data(db_data.clone())
             .service(add_person)
             .service(get_all_person)
             .service(get_person_by_id)
             .service(delete_person_by_id)
             .service(edit_person)
-
+            /* DIARY COLLECTION */
             .app_data(db_diary_data.clone())
-            .route("/{route:.*}", web::get().to(get_all::<Diary>))
-            .route("/{route:.*}", web::get().to(get_by_id::<Diary>))
-            .route("/{route:.*}", web::post().to(add::<Diary>))
-            .route("/{route:.*}", web::put().to(edit::<Diary>))
-            .route("/{route:.*}", web::delete().to(delete_by_id::<Diary>))
-
+            .route("/{route:.*}", web::get()    .to(get_all::       <Diary>))
+            .route("/{route:.*}", web::get()    .to(get_by_id::     <Diary>))
+            .route("/{route:.*}", web::post()   .to(add::           <Diary>))
+            .route("/{route:.*}", web::put()    .to(edit::          <Diary>))
+            .route("/{route:.*}", web::delete() .to(delete_by_id::  <Diary>))
+            /* DREAM COLLECTION */
             .app_data(db_dream_data.clone())
-            .route("/{route:.*}", web::get().to(get_all::<Dream>))
-            .route("/{route:.*}", web::get().to(get_by_id::<Dream>))
-            .route("/{route:.*}", web::post().to(add::<Dream>))
-            .route("/{route:.*}", web::put().to(edit::<Dream>))
-            .route("/{route:.*}", web::delete().to(delete_by_id::<Dream>))
+            .route("/{route:.*}", web::get()    .to(get_all::       <Dream>))
+            .route("/{route:.*}", web::get()    .to(get_by_id::     <Dream>))
+            .route("/{route:.*}", web::post()   .to(add::           <Dream>))
+            .route("/{route:.*}", web::put()    .to(edit::          <Dream>))
+            .route("/{route:.*}", web::delete() .to(delete_by_id::  <Dream>))
 
     })
         .bind(("127.0.0.1", 8080))?
